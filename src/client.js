e1af4fdd37dc157b42eb5b0cab3cb872d9d2b629
qwwe1 <113227915+qwwe1@users.noreply.github.com>
2022-11-10 13:44:58 +0600

    Create client.js

---
+++ src/client.js
@@ -0,0 +1,17 @@
+const Client = require('@structures/Client');
+
+const client = new Client();
+
+client.connect();
+
+module.exports = client;
+
+process.on('uncaughtException', (e) => {
+    client.logger.error(e);
+});
+process.on('unhandledRejection', (e) => {
+    client.logger.error(e);
+});
+process.on('warning', (e) => {
+    client.logger.warn(e);
+});
